70daecf87d251081c606f57d0d9dff2101dd4e4c
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-06-04 17:41:24 -0300

    Delete VarzeaCup directory

--- VarzeaCup/config/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include_once('connection.php');
-
-    $sql = "update partida set timea = '".$_REQUEST['timeA']"', timeb='sl2',golstimea = 1, golstimeb=2, horap='12:12:10', datap='121212',rodadap=1 where id = 3"; //aqui eu recebo o ID de qual elemento vou alterar 
-    $resultado = $conexao->query($sql);
-
-?>
