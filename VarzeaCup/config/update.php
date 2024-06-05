97fced4d49351ea0ed7d0530fdbdd8eab1cc35ab
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-06-05 17:57:56 -0300

    Delete VarzeaCup directory

--- VarzeaCup/config/update.php
+++
@@ -1,6 +0,0 @@
-<?php
-include_once('connection.php');
-
-$sql = "update partida set timea = '" . $_REQUEST['timeA'] . "',timeb = '" . $_REQUEST['timeB'] . "',golstimea = '" . $_REQUEST['golstimeA'] . "',golstimeb = '" . $_REQUEST['golstimeB'] . "', horap= '" . $_REQUEST['horaP'] . "', datap='" . $_REQUEST['dataP'] . "',rodadap='" . $_REQUEST['rodadaP'].'" where id = 3"';
-
-$resultado = $conexao->query($sql);
