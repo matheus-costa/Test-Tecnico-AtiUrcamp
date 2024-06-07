e84aed75c303eb4b85ac9e900ea51c73f84f343c
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-06-07 18:28:36 -0300

    Add files via upload

---
+++ VarzeaCup/exclui_partida.php
@@ -0,0 +1,26 @@
+<?php
+
+$sql = " delete from partida where id = '" . $_REQUEST['id'] . "' ";
+$conexao = new PDO('pgsql:host=localhost;port=5432;dbname=varzeacup;user= postgres; password=postgres');
+
+$resultado = $conexao->exec($sql);
+unset($conexao);
+if ($resultado) {
+?>
+<p>Removido com sucesso.</p>
+<script>
+setTimeout(function() {
+    window.location.assign('../lista_partida.php');
+}, 2000);
+</script>
+<?php
+} else {
+?>
+<p>Erro ao remover.</p>
+<script>
+setTimeout(function() {
+    window.history.back();
+}, 2000);
+</script>
+<?php
+}
