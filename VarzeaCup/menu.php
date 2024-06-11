bf9ff3f76e3b42d9aa783ea9f81cdc6c388595f9
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-06-11 17:48:32 -0300

    Add files via upload

---
+++ VarzeaCup/menu.php
@@ -0,0 +1,7 @@
+<p>
+    <link rel = "stylesheet" href = "style.css">
+    <a href="cadastro_partida.php">Cadastrar Partida</a>
+    <a href="lista_partida.php">Lista de Partidas</a>
+    <a href="tabela_jogos.php">Tabela de Jogos</a> 
+</p>
+
