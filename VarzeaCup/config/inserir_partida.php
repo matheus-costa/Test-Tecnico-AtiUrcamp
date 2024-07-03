a71db68e61236f9c0df567797d751730896e254a
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2024-07-03 18:06:52 -0300

    Add files via upload

---
+++ VarzeaCup/config/inserir_partida.php
@@ -0,0 +1,40 @@
+<?php
+
+$sql = " insert into partida values (default, '" .
+    $_REQUEST['timeA'] . "', '" .
+    $_REQUEST['timeB'] . "', '" .
+    $_REQUEST['golstimeA'] . "', '" .
+    $_REQUEST['golstimeB'] . "', '" .
+    $_REQUEST['horaP'] . "', '" .
+    $_REQUEST['dataP'] . "', '" .
+    $_REQUEST['rodadaP'] .
+    "' ); ";
+
+var_dump($sql);
+
+$conexao = new PDO('pgsql:host=localhost;port=5432;dbname=varzeacup;user= postgres;password=141222001');
+
+
+$resultado = $conexao->exec($sql);
+unset($conexao);
+
+
+if ($resultado) {
+?>
+<p>Inserido com sucesso.</p>
+<script>
+setTimeout(function() {
+    window.location.assign('lista_partida.php');
+}, 2000);
+</script>
+<?php
+} else {
+?>
+<p>Erro ao inserir.</p>
+<script>
+setTimeout(function() {
+    window.history.back();
+}, 2000);
+</script>
+<?php
+}
